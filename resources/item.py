703b3d1d0aec5bbe3bc64665ea761923ad986410
Prashantkg16 <prashantkg16@gmail.com>
2023-02-15 18:14:37 +0530

    Smorest - Schema implementation - marshmallo @blp.arguments(StoreSchema) @blp.response(201, StoreSchema)

--- resources/item.py
+++ resources/item.py
@@ -3,25 +3,20 @@ from flask import request
 from flask_smorest import Blueprint, abort
 from flask.views import MethodView
 from db import items
+from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("items", __name__, description="Operation on Items")
 
+
 @blp.route("/item")
 class ItemList(MethodView):
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
         return {"items": list(items.values())}
 
-    def post(self):
-        item_data = request.get_json()
-        if (
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ):
-            abort(
-                400,
-                message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
-            )
+    @blp.arguments(ItemSchema)
+    @blp.response(201, ItemSchema)
+    def post(self, item_data):
         for item in items.values():
             if (
                 item_data["name"] == item["name"]
@@ -35,8 +30,10 @@ class ItemList(MethodView):
 
         return item
 
+
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
         try:
             return items[item_id]
@@ -50,16 +47,10 @@ class Item(MethodView):
         except KeyError:
             abort(404, message="Item not found.")
 
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
     def put(self, item_id):
         item_data = request.get_json()
-        # There's  more validation to do here!
-        # Like making sure price is a number, and also both items are optional
-        # Difficult to do with an if statement...
-        if "price" not in item_data or "name" not in item_data:
-            abort(
-                400,
-                message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
-            )
         try:
             item = items[item_id]
             item |= item_data
--- schemas.py
+++ schemas.py
@@ -1,6 +1,5 @@
 from marshmallow import Schema, fields
 
-
 class ItemSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
